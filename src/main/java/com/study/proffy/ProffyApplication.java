e11a1a8495b9d38457e190e72ff355a3d23a6919
Caio Macedo <caiomacedo0102@gmail.com>
2022-10-14 20:08:45 -0300

    Configurações Iniciais
    Adicionado configurações do banco no application.properties
    Adicionado exclusão de auto configuração de banco no ProffyApplication.java
    Adicionado dependencias para testes no pom.xml
    Adicionado dependencias para tratamento de vulnerabilidades pom.xml

--- src/main/java/com/study/proffy/ProffyApplication.java
+++ src/main/java/com/study/proffy/ProffyApplication.java
@@ -2,8 +2,9 @@ package com.study.proffy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ProffyApplication {
 
     public static void main(String[] args) {
