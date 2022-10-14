97a271a45e88fd54192496ac4b5a964939e80088
Caio Macedo <caiomacedo0102@gmail.com>
2022-10-14 17:54:57 -0300

    Commit Inicial

---
+++ src/main/java/com/study/proffy/ProffyApplication.java
@@ -0,0 +1,13 @@
+package com.study.proffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProffyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProffyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/proffy/ProffyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.proffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProffyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
