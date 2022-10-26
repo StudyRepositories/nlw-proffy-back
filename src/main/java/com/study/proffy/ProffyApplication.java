b88d9ed5da83d5abf6f87fa8a81c0b8dcebd7141
Caio Macedo <caiomacedo0102@gmail.com>
2022-10-26 13:41:59 -0300

    Configurado Conexão Banco

--- src/main/java/com/study/proffy/ProffyApplication.java
+++ src/main/java/com/study/proffy/ProffyApplication.java
@@ -2,9 +2,8 @@ package com.study.proffy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ProffyApplication {
 
     public static void main(String[] args) {
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+--Inicializa o Schema principal da Aplicação
+create schema if not exists proffy;
