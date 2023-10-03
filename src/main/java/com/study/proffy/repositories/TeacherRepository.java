f5c3dd652788416a945b61f1798530dce59769d4
Caio Macedo <caiomacedo0102@gmail.com>
2023-10-03 19:01:43 -0300

    Ajustes no TeacherRepository.java

--- src/main/java/com/study/proffy/repositories/TeacherRepository.java
+++ src/main/java/com/study/proffy/repositories/TeacherRepository.java
@@ -2,17 +2,14 @@ package com.study.proffy.repositories;
 
 import com.study.proffy.entities.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface TeacherRepository extends CrudRepository<Teacher, UUID> {
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
     Teacher findByResource(UUID resource);
 
     void deleteByResource(UUID resource);
-
-    Boolean existsByResource(UUID resource);
 }
